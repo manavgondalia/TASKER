664b7062dd25dd029c87e2f31f1322100b6e6184
Manav Gondalia <72291135+manavgondalia@users.noreply.github.com>
2023-03-04 20:42:39 +0530

    Update Tasks.js

--- frontend/src/components/Tasks/Tasks.js
+++ frontend/src/components/Tasks/Tasks.js
@@ -58,7 +58,7 @@ const Tasks = () => {
                 {
                     tasks && tasks.map((task) => (
                         <div className={'container mb-2 task-item' + (task.completed ? " completed-task" : "")} key={task._id}  >
-                            <FormControlLabel onClick={() => completeTask(task._id)} style={{ maxWidth: 225 }} control={<Checkbox checked={task.completed} />} label={task.taskdetail} />
+                            <FormControlLabel style={{ maxWidth: 225 }} control={<Checkbox checked={task.completed} onClick={() => completeTask(task._id)} />} label={task.taskdetail} />
                             <IconButton className='delete-icon' variant='outlined' onClick={() => deleteTask(task)}>
                                 <RemoveCircleOutlineIcon fontSize='inherit' style={{ color: "red" }} />
                             </IconButton>
