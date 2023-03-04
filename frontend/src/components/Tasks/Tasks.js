a063d86227ecc43e72a08de045cb76258250cc98
Manav Gondalia <72291135+manavgondalia@users.noreply.github.com>
2023-03-04 20:36:00 +0530

    Update Tasks.js
    Updated the calls to backend

--- frontend/src/components/Tasks/Tasks.js
+++ frontend/src/components/Tasks/Tasks.js
@@ -1,8 +1,7 @@
-import { IconButton } from '@mui/material';
+import { Checkbox, IconButton, FormControlLabel } from '@mui/material';
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import AddTask from '../AddTask';
-import Task from './Task';
+import React, { useEffect, useState } from 'react';
+import AddTask from './AddTask';
 import './Task.css';
 import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,58 +10,58 @@ import Completed from './Completed';
 // const URL = "http://localhost:5000/tasks/"
 const URL = process.env.REACT_APP_SERVER_URL;
 
-const fetchHandler = async () => {
-    return await axios.get(URL).then((res) => res.data)
-}
-
 const Tasks = () => {
 
     const [tasks, setTasks] = useState();
 
     useEffect(() => {
-        fetchHandler().then(data => setTasks(data.tasks))
+        getTasks();
     }, []);
 
-    const handleCall = (childData) => {
+    const fetchHandler = async () => {
+        return await axios.get(URL).then((res) => res.data);
+    }
+    const getTasks = () => {
+        fetchHandler()
+            .then((data) => setTasks(data.tasks));
+    }
+
+    const addTask = (childData) => {
         const newlist = [...tasks, childData];
         setTasks(newlist);
     }
 
-    const handleCheck = (childData) => {
-        const newlist = tasks.map((taskitem) => {
-            if (taskitem._id === childData._id) {
-                const updatetaskitem = {
-                    ...taskitem,
-                    completed: !taskitem.completed,
-                };
-                return updatetaskitem;
+    const completeTask = async (id) => {
+        console.log("ID => ", id);
+        const updated_task = await axios.put(`${URL}${id}`).then(res => res.data);
+        console.log(updated_task.task);
+        setTasks(tasks => tasks.map(task => {
+            if (task._id === updated_task.task._id) {
+                task.completed = updated_task.task.completed;
             }
-            return taskitem;
-        });
+            return task;
+        }));
+    }
+
+    const deleteTask = async (task) => {
+        await axios.delete(`${URL}${task._id}`).then(res => res.data);
+        const newlist = tasks.filter((item) => item._id !== task._id);
         setTasks(newlist);
     }
 
     return (
-        <div className='text-align-center mx-auto mt-5 px-4 pb-1' style={{ maxHeight: 450, maxWidth: "fit-content", backgroundColor: "#F9ED69", borderRadius: 10, overflowY: "auto" }}>
+        <div className='card text-align-center mx-auto mt-5 px-4 pb-1 '>
             <ul className='ps-0'>
                 <div>
-                    <AddTask parentCall={handleCall} />
+                    <AddTask parentCall={addTask} />
                 </div>
                 {
-                    tasks && tasks.map((task, i) => (
-                        <div key={task._id} >
-                            <Task parentCall={handleCheck} task={task} list={tasks} element=
-                                {
-                                    <IconButton variant='outlined' onClick={
-                                        async () => {
-                                            const newlist = tasks.filter((item) => item._id !== task._id);
-                                            setTasks(newlist);
-                                            await axios.delete(`${URL}${task._id}`).then(res => res.data);
-                                        }}>
-                                        <RemoveCircleOutlineIcon fontSize='inherit' style={{ color: "red" }} />
-                                    </IconButton>
-                                }
-                            />
+                    tasks && tasks.map((task) => (
+                        <div className={'container mb-2 task-item' + (task.completed ? " completed-task" : "")} key={task._id}  >
+                            <FormControlLabel onClick={() => completeTask(task._id)} style={{ maxWidth: 225 }} control={<Checkbox checked={task.completed} />} label={task.taskdetail} />
+                            <IconButton className='delete-icon' variant='outlined' onClick={() => deleteTask(task)}>
+                                <RemoveCircleOutlineIcon fontSize='inherit' style={{ color: "red" }} />
+                            </IconButton>
                         </div>
                     ))
                 }
